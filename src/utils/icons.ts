8b3f1aa80ebe2c0767a62f6db4809c8a39912563
Renan Fischer <renan.fischer.2013@gmail.com>
2024-04-16 01:24:59 -0300

    Add XCircleIcon to icons.ts and x-circle.svg to images/icons

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -3,10 +3,12 @@ import AccountIcon from "@/images/icons/account.svg";
 import ShoppingCardIcon from "@/images/icons/shopping-cart.svg";
 import XIcon from "@/images/icons/x.svg";
 import MagnifyingGlassIcon from "@/images/icons/magnifying-glass.svg";
+import XCircleIcon from "@/images/icons/x-circle.svg";
 export {
   SearchIcon,
   AccountIcon,
   ShoppingCardIcon,
   XIcon,
   MagnifyingGlassIcon,
+  XCircleIcon,
 };
