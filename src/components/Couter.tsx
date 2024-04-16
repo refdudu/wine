8d0ca1c6dabcfe91e0712d40d3fc3c5507e8300d
Renan Fischer <renan.fischer.2013@gmail.com>
2024-04-16 01:25:07 -0300

    Add Counter component for handling increment and decrement functionality

---
+++ src/components/Couter.tsx
@@ -0,0 +1,18 @@
+interface CounterProps {
+  handleAdd: () => void;
+  handleRemove: () => void;
+  total: number;
+}
+export function Counter({ handleAdd, handleRemove, total }: CounterProps) {
+  return (
+    <div className="border border-custom-gray rounded-md flex gap-4 px-2 py-1">
+      <button className="text-custom-gray" onClick={handleRemove}>
+        -
+      </button>
+      <span className="text-custom-gray-light">{total}</span>
+      <button className="text-custom-gray" onClick={handleAdd}>
+        +
+      </button>
+    </div>
+  );
+}
