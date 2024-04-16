bd4447cbe69afda2ace00ebed3a44cda393b3b6d
Renan Fischer <renan.fischer.2013@gmail.com>
2024-04-16 01:25:03 -0300

    Refactor Button component to remove unnecessary text and use self-closing tag

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,8 +5,6 @@ export function Button(props: HTMLAttributes<HTMLButtonElement>) {
     <button
       {...props}
       className={`bg-custom-green filter hover:brightness-110 transition flex-1 py-2 font-lato text-white rounded-sm ${props.className}`}
-    >
-      Adicionar
-    </button>
+    />
   );
 }
