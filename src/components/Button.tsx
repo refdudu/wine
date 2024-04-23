b3d39c030c76d0dab881bc5ea97a0969fa749052
Renan Fischer <renanfisher@atendare.com>
2024-04-23 18:33:19 -0300

    adição de productCardSkeleton

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ export function Button(props: HTMLAttributes<HTMLButtonElement>) {
 	return (
 		<button
 			{...props}
-			className={`bg-custom-green filter hover:brightness-110 transition flex-1 py-2 font-lato text-white rounded-sm ${props.className}`}
+			className={`bg-custom-green filter hover:brightness-110 transition w-full py-2 font-lato text-white rounded-sm ${props.className}`}
 		/>
 	);
 }
--- src/components/pages/Home/LargeLayout.tsx
+++ src/components/pages/Home/LargeLayout.tsx
@@ -2,7 +2,7 @@ import { useHomeLarge } from "./useHomeLarge";
 import { SideBar } from "@/components/pages/Home/Sidebar";
 import { Pagination } from "@/components/Pagination";
 import { ProductsGridLayout } from "@/components/pages/Home/ProductsGridLayout";
-import { ProductCard } from "./ProductCard";
+import { ProductCard, ProductCardSkeleton } from "./ProductCard";
 import { useShoppingCart } from "@/contexts/ShoppingCartContext";
 import type { ProductI } from "@/interfaces/ProductI";
 
@@ -11,48 +11,79 @@ import type { ProductI } from "@/interfaces/ProductI";
 // }
 
 export function LargeLayout() {
-	const { betweenPrices, handleFilterBetweenPrices, handleFilterSearch, pageIndex, productsResponse, searchText, setPageIndex, isFetching } =
-		useHomeLarge();
-	return (
-		<>
-			<SideBar betweenPrices={betweenPrices} changeBetweenPrice={handleFilterBetweenPrices} />
-			<>
-				<ProductsGridLayout
-					isLoading={isFetching}
-					searchText={searchText}
-					handleFilterSearch={handleFilterSearch}
-					footer={
-						productsResponse && (
-							<div className="lg:flex justify-center">
-								<Pagination
-									current={pageIndex}
-									changePageIndex={(page) => {
-										window.scrollTo(0, 0);
-										setPageIndex(page);
-									}}
-									total={Math.ceil(productsResponse.total / productsResponse.pageSize)}
-								/>
-							</div>
-						)
-					}
-					totalProducts={productsResponse?.total || 0}>
-					<ProductGrid products={productsResponse?.products} />
-				</ProductsGridLayout>
-			</>
-		</>
-	);
+  const {
+    betweenPrices,
+    handleFilterBetweenPrices,
+    handleFilterSearch,
+    pageIndex,
+    productsResponse,
+    searchText,
+    setPageIndex,
+    isFetching,
+  } = useHomeLarge();
+  return (
+    <>
+      <SideBar
+        betweenPrices={betweenPrices}
+        changeBetweenPrice={handleFilterBetweenPrices}
+      />
+      <>
+        <ProductsGridLayout
+          isLoading={isFetching && !productsResponse}
+          searchText={searchText}
+          handleFilterSearch={handleFilterSearch}
+          footer={
+            productsResponse && (
+              <div className="lg:flex justify-center">
+                <Pagination
+                  current={pageIndex}
+                  changePageIndex={(page) => {
+                    window.scrollTo(0, 0);
+                    setPageIndex(page);
+                  }}
+                  total={Math.ceil(
+                    productsResponse.total / productsResponse.pageSize
+                  )}
+                />
+              </div>
+            )
+          }
+          totalProducts={productsResponse?.total || 0}
+        >
+          <ProductGrid
+            isLoading={isFetching}
+            products={productsResponse?.products}
+          />
+        </ProductsGridLayout>
+      </>
+    </>
+  );
 }
 interface ProductGridProps {
-	products?: ProductI[];
+  products?: ProductI[];
+  isLoading: boolean;
 }
-function ProductGrid({ products }: ProductGridProps) {
-	const { handleAddInShoppingCart } = useShoppingCart();
-	if (!products) return <></>;
-	return (
-		<>
-			{products.map((product) => (
-				<ProductCard key={product.name} {...{ product }} onAdd={() => handleAddInShoppingCart(product)} />
-			))}
-		</>
-	);
+function ProductGrid({ products, isLoading }: ProductGridProps) {
+  const { handleAddInShoppingCart } = useShoppingCart();
+  if (!products) return <></>;
+  if (isLoading) {
+    return (
+      <>
+        {Array.from({ length: 9 }, (_, i) => i).map((key) => (
+          <ProductCardSkeleton key={key} />
+        ))}
+      </>
+    );
+  }
+  return (
+    <>
+      {products.map((product) => (
+        <ProductCard
+          key={product.name}
+          {...{ product }}
+          onAdd={() => handleAddInShoppingCart(product)}
+        />
+      ))}
+    </>
+  );
 }
