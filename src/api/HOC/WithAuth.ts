916adec8dcb07f818910b1c051683bccd1ffe8c2
Renan Fischer <renanfisher@atendare.com>
2024-05-17 07:52:46 -0300

    comment signIn

--- src/api/HOC/WithAuth.ts
+++ src/api/HOC/WithAuth.ts
@@ -4,6 +4,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 export interface ApiRequestAuth extends NextApiRequest {
   userUid: string;
 }
+
+//TODO isso aqui é um middleware
+// Deve ser criado um WithAuth para buscar o usuário ssr
 export const WithAuth = async (
   main: (req: ApiRequestAuth, res: NextApiResponse) => void,
   req: NextApiRequest,
