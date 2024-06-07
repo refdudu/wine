4ba496dcfa52446fcb384f2b5529d30c9b87e807
Renan Fischer <renan.fischer.2013@gmail.com>
2024-06-07 19:16:25 -0300

    chore: Refactor BuyPage component structure for improved readability

--- src/components/pages/Buy/index.tsx
+++ src/components/pages/Buy/index.tsx
@@ -6,21 +6,36 @@ export function BuyPage() {
     <div className="flex flex-col h-screen">
       <BuyHeader />
 
-      <main>
-        <div>
-          <span>Cadastrar novo endereço</span>
-        </div>
-        <div>
+      <div className="w-full py-10 mx-auto px-3 overflow-auto h-full">
+        <main className="max-w-[1120px] flex mx-auto">
           <div>
             <div>
-                <div/>
-                <span>Casa</span>
-                <span>Rua Emilio Tesche, 782, Oriental - Três de Maio, RS - CEP 98910-000</span>
+              <span>Cadastrar novo endereço</span>
+            </div>
+            <div>
+              <FirstColumn />
             </div>
           </div>
-          <div></div>
-        </div>
-      </main>
-    </div>  
+        </main>
+      </div>
+    </div>
+  );
+}
+function FirstColumn() {
+  return (
+    <div>
+      <LocationCard />
+    </div>
+  );
+}
+function LocationCard() {
+  return (
+    <div className="flex flex-col">
+      <div />
+      <span>Casa</span>
+      <span>
+        Rua Emilio Tesche, 782, Oriental - Três de Maio, RS - CEP 98910-000
+      </span>
+    </div>
   );
 }
