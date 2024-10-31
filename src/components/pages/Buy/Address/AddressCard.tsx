24be0994d78a32366e36756cd567688b82955b1e
Renan Fischer <renan.fischer.2013@gmail.com>
2024-10-30 22:12:56 -0300

    Refactor AddressCard component by extracting Header, Content, AddressHeader, and AddressDetails into separate functions for improved readability and maintainability

--- src/components/pages/Buy/Address/AddressCard.tsx
+++ src/components/pages/Buy/Address/AddressCard.tsx
@@ -9,6 +9,29 @@ interface AddressCardProps {
   isSelected: boolean;
   selectAddress: () => void;
 }
+
+interface HeaderProps {
+  isSelected: boolean;
+}
+
+interface ContentProps {
+  address: AddressI;
+  isSelected: boolean;
+  selectAddress: () => void;
+  setIsEditing: () => void;
+}
+
+interface AddressHeaderProps {
+  address: AddressI;
+  isSelected: boolean;
+  selectAddress: () => void;
+  setIsEditing: () => void;
+}
+
+interface AddressDetailsProps {
+  address: AddressI;
+}
+
 export function AddressCard({
   setIsEditing,
   address,
@@ -17,36 +40,82 @@ export function AddressCard({
 }: AddressCardProps) {
   return (
     <div className="w-full border border-custom-gray-light">
-      <header
-        className={`${classNames({
-          "bg-custom-violet": isSelected,
-        })} w-full text-white h-8 flex items-center px-4`}
-      >
-        {isSelected && "Selecionado"}
-      </header>
-      <div className="p-4 flex flex-col justify-center">
-        <div className="flex items-center text-xl justify-between">
-          <div className="flex gap-2 items-center">
-            <span>{address.addressIdentify}</span>
-            {!isSelected && (
-              <button
-                onClick={selectAddress}
-                className="text-custom-violet text-sm"
-              >
-                Selecionar
-              </button>
-            )}
-          </div>
-          <button onClick={setIsEditing}>
-            <PencilLine color="#B6116E" />
-          </button>
-        </div>
-        <div className="flex items-center text-custom-gray-light font-light gap-1">
-          <User />
-          {address.addressIdentify}
-        </div>
-        <AddressText address={address} />
-      </div>
+      <Header isSelected={isSelected} />
+      <Content
+        address={address}
+        isSelected={isSelected}
+        selectAddress={selectAddress}
+        setIsEditing={setIsEditing}
+      />
     </div>
+  );
+}
+
+function Header({ isSelected }: HeaderProps) {
+  return (
+    <header
+      className={classNames("w-full text-white h-8 flex items-center px-4", {
+        "bg-custom-violet": isSelected,
+      })}
+    >
+      {isSelected && "Selecionado"}
+    </header>
+  );
+}
+
+function Content({
+  address,
+  isSelected,
+  selectAddress,
+  setIsEditing,
+}: ContentProps) {
+  return (
+    <div className="p-4 flex flex-col justify-center">
+      <AddressHeader
+        address={address}
+        isSelected={isSelected}
+        selectAddress={selectAddress}
+        setIsEditing={setIsEditing}
+      />
+      <AddressDetails address={address} />
+    </div>
+  );
+}
+
+function AddressHeader({
+  address,
+  isSelected,
+  selectAddress,
+  setIsEditing,
+}: AddressHeaderProps) {
+  return (
+    <div className="flex items-center text-xl justify-between">
+      <div className="flex gap-2 items-center">
+        <span>{address.addressIdentify}</span>
+        {!isSelected && (
+          <button
+            onClick={selectAddress}
+            className="text-custom-violet text-sm"
+          >
+            Selecionar
+          </button>
+        )}
+      </div>
+      <button onClick={setIsEditing}>
+        <PencilLine color="#B6116E" />
+      </button>
+    </div>
+  );
+}
+
+function AddressDetails({ address }: AddressDetailsProps) {
+  return (
+    <>
+      <div className="flex items-center text-custom-gray-light font-light gap-1">
+        <User />
+        {address.addressIdentify}
+      </div>
+      <AddressText address={address} />
+    </>
   );
 }
