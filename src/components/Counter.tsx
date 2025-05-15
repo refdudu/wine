6a0778e5968a5f83f0698a74a121b86ae9075b77
Renan Fischer <renan.fischer.2013@gmail.com>
2025-05-14 21:51:07 -0300

    feat: página de detalhes do produto

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -16,3 +16,33 @@ export function Counter({ handleAdd, handleRemove, total }: CounterProps) {
     </div>
   );
 }
+export function ButtonCounter({
+  handleAdd,
+  handleRemove,
+  total,
+}: CounterProps) {
+  return (
+    <div className=" rounded-md bg-custom-green text-white flex gap-8 px-6 py-3 max-w-80 font-bold">
+      <div className="flex gap-4 items-center">
+        <button
+          type="button"
+          className="border rounded-full border-white w-6 h-6 flex items-center justify-center"
+          onClick={handleRemove}
+        >
+          -
+        </button>
+        <span className="">{total}</span>
+        <button
+          type="button"
+          className="border rounded-full border-white w-6 h-6 flex items-center justify-center"
+          onClick={handleAdd}
+        >
+          +
+        </button>
+      </div>
+      <button type='button' className="flex-1 flex items-center justify-center">
+        <span className="font-bold">Adicionar</span>
+      </button>
+    </div>
+  );
+}
--- src/components/pages/Product/ProductDetails.tsx
+++ src/components/pages/Product/ProductDetails.tsx
@@ -1,9 +1,9 @@
-import { Counter } from "@/components/Counter";
+import { ButtonCounter, Counter } from "@/components/Counter";
 import { Layout } from "@/components/Layout";
 import type { ProductI } from "@/interfaces/ProductI";
 import { ApiProductService } from "@/services/ProductsService";
 import { api } from "@/utils/api";
-import { CaretLeft, CaretRight } from "@phosphor-icons/react";
+import { CaretLeft, CaretRight, Images, Star } from "@phosphor-icons/react";
 import type { GetServerSideProps, GetStaticProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
@@ -12,70 +12,114 @@ import { useParams } from "next/navigation";
 interface ProductDetailsProps {
   product: ProductI;
 }
+const image =
+  "https://images.tcdn.com.br/img/img_prod/796852/vinho_tinto_suave_bordo_san_martin_1l_153_1_20200525112308.png";
 export function ProductDetails({ product }: ProductDetailsProps) {
   console.log({ product });
 
   return (
     <Layout>
-      <div className="w-full max-w-[1120px] py-10 mx-auto px-3 overflow-auto h-full">
-        <header className="mb-4">
-          <Link className="flex items-center gap-2" href="/">
-            <CaretLeft width={16} />
-            <span>Voltar</span>
-          </Link>
-        </header>
-        <main className="flex">
-          <div className="flex-1">
-            <div className="h-64 w-full bg-gray-200" />
-            {/* <Image alt="Produlct Image" src={"https://example.com/image.jpg"} /> */}
-          </div>
-          <div className="flex-1">
-            <header className="flex gap-2 items-center mb-4 text-custom-violet">
-              <span>Vinhos</span>
-              <CaretRight width={16} className="text-custom-gray-light" />
-              <span>Estados Unidos</span>
-            </header>
-            <h1 className="text-3xl font-bold mb-8">Nome do Produto</h1>
-
-            <div className="mb-1 flex gap-2 items-end">
-              <span className="text-custom-gray-dark md:text-2xs text-xs uppercase">
-                Sócio wine
-              </span>
-              <span className="text-custom-violet md:text-2xs text-xs">
-                R${" "}
-                <span className="md:text-xl text-lg">
-                  {/* {partnerPriceFormatted.integer} */}
-                </span>
-                {/* ,{partnerPriceFormatted.decimal} */}
-              </span>
+      <div className="w-full py-10 mx-auto px-3 overflow-auto h-full">
+        <div className="max-w-[1120px] mx-auto">
+          <Header />
+          <main className="flex">
+            <div className="flex flex-1 justify-center">
+              <img className="w-64" alt="" src={image} />
             </div>
-            <span className="md:text-2xs text-xs text-custom-gray uppercase">
-              {/* Não sócio {priceFormatted} */}
-            </span>
-            <div>
-              <span>Descrição do produto</span>
-              <span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                euismod ante eu lacus vulputate hendrerit. Integer eget lectus
-                eget mauris tincidunt dictum. Praesent vulputate justo quis
-                tincidunt fringilla. Aliquam erat volutpat. Sed efficitur ipsum
-                eget justo semper, id pulvinar libero tincidunt. Curabitur
-                hendrerit dolor eu justo laoreet, sed finibus quam vehicula.
-                Nullam eget tincidunt nunc. Sed auctor, urna eu tristique
-                tincidunt, nunc nisl semper dolor, eu ultrices nisl nunc eu
-                nunc. Sed do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua.
-              </span>
+            <Details />
+          </main>
+          <div className="border-t border-custom-gray-light mt-8 pt-8">
+            <span>Avaliações dos clientes</span>
+            <div className="">
+              <ClientRating />
             </div>
-            <Counter
-              handleAdd={() => console.log}
-              handleRemove={() => console.log}
-              total={0}
-            />
           </div>
-        </main>
+        </div>
       </div>
     </Layout>
+  );
+}
+
+function ClientRating() {
+  return (
+    <div className="">
+      <div className="flex gap-2 items-center">
+        <div className="w-8 h-8 rounded-full bg-black" />
+        <span>Nome do cliente</span>
+      </div>
+      <div>
+        <RatingStars total={5} />
+        <span className="font-bold">Muito show esse vinho</span>
+      </div>
+      <span>Data da avaliação</span>
+    </div>
+  );
+}
+interface RatingStarsProps {
+  total: number;
+}
+function RatingStars({ total }: RatingStarsProps) {
+  return (
+    <div className="flex gap-1">
+      {Array.from({ length: 5 }, (_, i) => i + 1).map((i) => (
+        <Star
+          key={i}
+          size={16}
+          weight={total >= i ? "fill" : "regular"}
+          color="#FBBF24"
+        />
+      ))}
+    </div>
+  );
+}
+function Details() {
+  return (
+    <div className="flex-1">
+      <header className="flex gap-2 items-center mb-4 text-custom-violet">
+        <span>Vinhos</span>
+        <CaretRight width={16} className="text-custom-gray-light" />
+        <span>Estados Unidos</span>
+      </header>
+      <h1 className="text-3xl font-bold">Nome do Produto</h1>
+
+      <div className="my-8 flex flex-col gap-1">
+        <span className="text-custom-violet font-bold text-2xl">
+          R$ <span className="text-4xl">20</span>,20
+        </span>
+        <span className="text-custom-gray-light md:text-2xs font-bold text-xs uppercase">
+          não sócio wine R$ 20,20
+        </span>
+      </div>
+      <div className="flex flex-col gap-2 mb-8">
+        <span className="font-bold text-lg">Descrição do produto</span>
+        <span className="text-custom-gray-light">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+          ante eu lacus vulputate hendrerit. Integer eget lectus eget mauris
+          tincidunt dictum. Praesent vulputate justo quis tincidunt fringilla.
+          Aliquam erat volutpat. Sed efficitur ipsum eget justo semper, id
+          pulvinar libero tincidunt. Curabitur hendrerit dolor eu justo laoreet,
+          sed finibus quam vehicula. Nullam eget tincidunt nunc. Sed auctor,
+          urna eu tristique tincidunt, nunc nisl semper dolor, eu ultrices nisl
+          nunc eu nunc. Sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua.
+        </span>
+      </div>
+      <ButtonCounter
+        handleAdd={() => console.log}
+        handleRemove={() => console.log}
+        total={0}
+      />
+    </div>
+  );
+}
+function Header() {
+  return (
+    <header className="mb-4">
+      <Link className="flex items-center gap-2" href="/">
+        <CaretLeft width={16} />
+        <span>Voltar</span>
+      </Link>
+    </header>
   );
 }
 export const getServerSideProps: GetServerSideProps<
@@ -95,9 +139,9 @@ export const getServerSideProps: GetServerSideProps<
       }, // Mock data for product
     },
   };
-//   if (!params) {
-//     return { props: { product: undefined }, redirect: { destination: "/" } };
-//   }
+  //   if (!params) {
+  //     return { props: { product: undefined }, redirect: { destination: "/" } };
+  //   }
   //   const productService = new ApiProductService();
   //   const productId = params.productId; // Use the productId from params
   //   const product = await productService.getProduct(productId);
